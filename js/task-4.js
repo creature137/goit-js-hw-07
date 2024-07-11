b50bedd60b0ce235d70fe87303ae06db073e535c
creature137 <xoxorich@proton.me>
2024-07-11 11:46:31 +0300

    -1000 aura

--- js/task-4.js
+++ js/task-4.js
@@ -1,6 +1,3 @@
-// 3. если форма заполнена и отправлена, собрать приколы с формы в объект где ключ это имя инпута изначение это значеия этих инпутом без побелов
-// 3.1
-
 const form = document.querySelector(".login-form");
 const inputElements = [...form.querySelectorAll("input")];
 
